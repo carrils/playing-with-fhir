468a410711c83eaea465a2d9c2b68c9f1c2e8e9e
scarrillo <scarrillo@mitre.org>
2023-02-27 17:11:56 -0600

    add fhirdrills AU example and start example patient #1's creation

--- src/main/java/org/mitre/phfic/Main.java
+++ src/main/java/org/mitre/phfic/Main.java
@@ -11,11 +11,9 @@ import ca.uhn.fhir.util.BundleUtil;
 import org.hl7.fhir.instance.model.api.IBase;
 import org.hl7.fhir.instance.model.api.IBaseBundle;
 import org.hl7.fhir.instance.model.api.IBaseResource;
-import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.*;
 import org.hl7.fhir.r4.model.ContactPoint.ContactPointSystem;
 import org.hl7.fhir.r4.model.ContactPoint.ContactPointUse;
-import org.hl7.fhir.r4.model.Enumerations;
-import org.hl7.fhir.r4.model.Patient;
 import org.json.simple.JSONObject;
 
 import java.io.File;
@@ -29,49 +27,57 @@ import java.util.UUID;
 
 //logback output levels (asc precedence): TRACE, DEBUG, INFO, WARN, ERROR
 
+/*
+    ToDo:
+    Create example resources in hapi fhir for
+        - US Core Patient [ ]
+        - US Core Condition [ ]
+        - US Core Encounter [ ]
+        - US Core Location [ ]
+        - US Core QuestionResponse [ ]
+        - US Core CarePlan [ ]
+        - Specimen Resource [ ]
+
+    Figure out which profile refers to which and double check you built the above correctly [ ]
+
+    Upload them to hapi fhir test server [ ]
+
+    Retrieve all with a patient?id=<whatever>$everything server interaction [ ]
+ */
 public class Main {
     public static void main(String[] args) {
         String serverBase = "http://hapi.fhir.org/baseR4"; // the server base url for your server
         FhirContext contextR4 = FhirContext.forR4();
 
-        // create a client factory for the above settings
+        // create a client factory
         IRestfulClientFactory clientFactory = contextR4.getRestfulClientFactory();
-
-        // set client settings
+        // increase timeouts since the server might be powered down
         clientFactory.setServerValidationMode(ServerValidationModeEnum.NEVER);
-        clientFactory.setConnectTimeout(20 * 1000);
+        clientFactory.setConnectTimeout(60 * 1000);
         clientFactory.setSocketTimeout(60 * 1000);
-
         // create logging interceptor
         LoggingInterceptor loggingInterceptor = new LoggingInterceptor();
         loggingInterceptor.setLogRequestSummary(true);
         loggingInterceptor.setLogRequestBody(true);
         loggingInterceptor.setLogRequestHeaders(true);
-
         // create a generic client
         IGenericClient client = contextR4.newRestfulGenericClient(serverBase);
-
         // register the logging interceptor
         client.registerInterceptor(loggingInterceptor);
 
-        List<IBaseResource> patients = new ArrayList<>(); // a container for the results of the search
+        // create the patient as close as possible to us core then manually conform it.
+        // upload that. after the patient resource you will have to try and create a resource for every profile
+        // that is listed in the dictionary, modify it to fit us core, and upload it to hapi fhir referring the og patient.
+        // they will all reference us core patient as fhir r4 does the same, so in theory
+        // once we create each resource, and point it at the patient we created, we should be
+        // able to pull it via a $everything server interaction for simplicity, on the patient?id=<whatever>$everything
 
-        // Perform the search. client.search() returns a bundle hence the wierd notation...
-        Bundle smith_bundle = client.search()
-                .forResource(Patient.class)
-                .where(Patient.NAME.matches().value("smith"))
-                .returnBundle(Bundle.class)
-                .execute();
+        Patient pat = new Patient();
+        HumanName name = pat.addName().setFamily("Aquato").addGiven("Razputin").addGiven("E");
+        Identifier identifier = pat.addIdentifier().setSystem("MEDSS").setValue("45D783B1");
 
-        // print the results of the search
-        // System.out.println("smith_bundle: \n" + smith_bundle); // org.hl7.fhir.r4.model.Bundle@78e16155
-
-        // add the returned bundle to a List data structure for storage
-        patients.addAll(BundleUtil.toListOfResources(contextR4, smith_bundle));
-        // System.out.println("'patients' List: \n" + patients); // [org.hl7.fhir.r4.model.Patient@54a3ab8f, org.hl7.fhir.r4.model.Patient@1968a49c, ... ]
-
-        // Print search result count
-        System.out.println("Found " + patients.size() + " patients with last name smith");
+        ContactPoint contact = pat.addTelecom();
+        contact.setUse(ContactPointUse.HOME);
 
 
     }
