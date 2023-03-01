e8024e87f6307c0284162ae1b7ad65167087a9a3
scarrillo <scarrillo@mitre.org>
2023-03-01 11:20:54 -0600

    initial patient upload to hapi fhir and extensions

--- src/main/java/org/mitre/phfic/Main.java
+++ src/main/java/org/mitre/phfic/Main.java
@@ -1,7 +1,9 @@
 package org.mitre.phfic;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.DataFormatException;
 import ca.uhn.fhir.parser.IParser;
+import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.api.IRestfulClientFactory;
 import ca.uhn.fhir.rest.client.api.ServerValidationModeEnum;
@@ -16,21 +18,15 @@ import org.hl7.fhir.r4.model.ContactPoint.ContactPointSystem;
 import org.hl7.fhir.r4.model.ContactPoint.ContactPointUse;
 import org.json.simple.JSONObject;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
+import java.io.*;
+import java.util.*;
 
 //logback output levels (asc precedence): TRACE, DEBUG, INFO, WARN, ERROR
 
 /*
     ToDo:
     Create example resources in hapi fhir for
-        - US Core Patient [ ]
+        - US Core Patient [-]
         - US Core Condition [ ]
         - US Core Encounter [ ]
         - US Core Location [ ]
@@ -45,7 +41,7 @@ import java.util.UUID;
     Retrieve all with a patient?id=<whatever>$everything server interaction [ ]
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String serverBase = "http://hapi.fhir.org/baseR4"; // the server base url for your server
         FhirContext contextR4 = FhirContext.forR4();
 
@@ -65,20 +61,64 @@ public class Main {
         // register the logging interceptor
         client.registerInterceptor(loggingInterceptor);
 
-        // create the patient as close as possible to us core then manually conform it.
-        // upload that. after the patient resource you will have to try and create a resource for every profile
-        // that is listed in the dictionary, modify it to fit us core, and upload it to hapi fhir referring the og patient.
-        // they will all reference us core patient as fhir r4 does the same, so in theory
-        // once we create each resource, and point it at the patient we created, we should be
-        // able to pull it via a $everything server interaction for simplicity, on the patient?id=<whatever>$everything
 
+        // [PATIENT RESOURCE]
         Patient pat = new Patient();
         HumanName name = pat.addName().setFamily("Aquato").addGiven("Razputin").addGiven("E");
         Identifier identifier = pat.addIdentifier().setSystem("MEDSS").setValue("45D783B1");
+        ContactPoint telecom = pat.addTelecom().setSystem(ContactPointSystem.PHONE).setUse(ContactPointUse.HOME).setValue("488 715-9846");
+        pat.setGender(Enumerations.AdministrativeGender.MALE).setBirthDate(new Date(92, 8, 12));
+        Address address = pat.addAddress().setUse(Address.AddressUse.HOME).addLine("101 main str").setCity("Minneapolis").setState("MN").setPostalCode("55403");
+        Communication communication = (Communication) new Communication().setLanguage("en");
+        // StringType is just a FHIR String data type, proprietary to the HAPI FHIR API. Race extension
+        pat.addExtension("http://hl7.org/fhir/us/core/StructureDefinition/us-core-race", new StringType("Asian"));
+        // Ethnicity extension
+        pat.addExtension("http://hl7.org/fhir/us/core/StructureDefinition/us-core-ethnicity", new StringType("Not Hispanic or Latino"));
+        // Gender Identity extension
+        pat.addExtension("http://hl7.org/fhir/us/core/STU5.0.1/StructureDefinition-us-core-genderIdentity.html", new StringType("M"));
+        // Birthsex Extension
+        pat.addExtension("http://hl7.org/fhir/us/core/STU5.0.1/StructureDefinition-us-core-birthsex.html", new StringType("M"));
 
-        ContactPoint contact = pat.addTelecom();
-        contact.setUse(ContactPointUse.HOME);
+        // [ENCODE]
+        IParser jsonParser = contextR4.newJsonParser().setPrettyPrint(true);
+        String patEncoded = jsonParser.encodeResourceToString(pat);
+        System.out.println("[json patient]\n" + patEncoded + "\n");
+
+        // [PRINT]
+        try {
+            Writer outf = new FileWriter("generated-patient.json");
+            outf.write(patEncoded);
+            outf.close();
+        } catch (Exception e) {
+            System.out.println("Well now look what you've done. How do we explain this to corporate.");
+            e.getStackTrace();
+        }
 
 
+         // [UPLOAD]
+        System.out.println("Press Enter to send the patient to server: " + serverBase);
+        System.in.read();
+
+        try {
+            // send resource up to the server - the result is stored in outcome hence the weird notation
+            MethodOutcome outcome = client.create()
+                    .resource(pat)
+                    .prettyPrint()
+                    .encodedJson()
+                    .execute();
+            // Retrieve id from outcome
+            IdType id = (IdType) outcome.getId();
+            System.out.println("Resource is available at: " + id.getValue());
+
+            // System.out.println("[OUTCOME]\n" + outcome.toString());
+
+            // Retrieve patient from outcome
+            Patient receivedPatient = (Patient) outcome.getResource();
+            System.out.println("This is what we sent up: \n" + jsonParser.encodeResourceToString(pat) + "\n");
+            System.out.println("And this is what we received back: \n" + jsonParser.encodeResourceToString(receivedPatient));
+        } catch (DataFormatException e) {
+            System.out.println("An error occurred trying to upload:");
+            e.printStackTrace();
+        }
     }
 }
